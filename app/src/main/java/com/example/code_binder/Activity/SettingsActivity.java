a91ce9eead5d79f31a6bca8a2ea56a8583192ada
komead <kapitanDG@mail.ru>
2024-04-17 15:55:24 +0300

    Улучшена работа с настройками

--- app/src/main/java/com/example/code_binder/Activity/SettingsActivity.java
+++ app/src/main/java/com/example/code_binder/Activity/SettingsActivity.java
@@ -1,12 +1,16 @@
-package com.example.code_binder;
+package com.example.code_binder.Activity;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.code_binder.Preferences;
+import com.example.code_binder.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SettingsActivity extends AppCompatActivity {
     private FloatingActionButton back_btn;
@@ -14,27 +18,31 @@ public class SettingsActivity extends AppCompatActivity {
     private EditText ip_et;
     private EditText port_et;
 
-    private SharedPreferences preferences;
+    private Preferences preferences;
+    private Map<String, String> settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        preferences = getSharedPreferences("ApplicationPrefs", MODE_PRIVATE);
-
         back_btn = findViewById(R.id.btn_back);
         ip_et = findViewById(R.id.et_ip);
         port_et = findViewById(R.id.et_port);
         save_btn = findViewById(R.id.btn_save);
+
+        preferences = new Preferences(this);
+        settings = new HashMap<>();
 
         setListeners();
         getSettings();
     }
 
     private void getSettings() {
-        ip_et.setText(preferences.getString("hostIP", "10.162.0.133"));
-        port_et.setText(preferences.getString("hostPort", "11000"));
+        Map<String, String> oldSettings = preferences.getAll();
+
+        ip_et.setText(oldSettings.get("hostIP"));
+        port_et.setText(oldSettings.get("hostPort"));
     }
 
     private void setListeners() {
@@ -48,10 +56,9 @@ public class SettingsActivity extends AppCompatActivity {
         save_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putString("hostIP", ip_et.getText().toString());
-                editor.putString("hostPort", port_et.getText().toString());
-                editor.apply();
+                settings.put("hostIP", ip_et.getText().toString());
+                settings.put("hostPort", port_et.getText().toString());
+                preferences.saveAll(settings);
             }
         });
     }
